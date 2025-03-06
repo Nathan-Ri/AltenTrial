9b668387b65dc07a5e5facfde48079c5306effd0
Nathan-Ri <nathan.richon@gmail.com>
2025-03-06 18:52:00 +0100

    Update UserController.php

--- back/Alten-eshop/src/Controller/UserController.php
+++ back/Alten-eshop/src/Controller/UserController.php
@@ -19,8 +19,8 @@ final class UserController extends AbstractController
     #[Route('/api/users', name: 'list', methods: ['GET'])]
     public function list(UserRepository $userRepository, SerializerInterface $serializer): JsonResponse
     {
-        $products = $userRepository->findAll();
-        $data = $serializer->serialize($products, 'json', ['groups' => 'user:read']);
+        $users = $userRepository->findAll();
+        $data = $serializer->serialize($users, 'json', ['groups' => 'user:read']);
         return new JsonResponse($data, Response::HTTP_OK, [], true);
     }
 
@@ -32,8 +32,20 @@ final class UserController extends AbstractController
         UserPasswordHasherInterface $passwordHasher
     ): Response
     {
-        $user = $serializer->deserialize($request->getContent(), User::class, 'json');
+        try {
+            $user = $serializer->deserialize($request->getContent(), User::class, 'json');
+        } catch (\Exception $e) {
+            return new JsonResponse(['error' => 'Données invalides'], Response::HTTP_BAD_REQUEST);
+        }
+        
+        if (!$user->getEmail() || !$user->getPassword()) {
+            return new JsonResponse(['error' => 'Email et mot de passe requis'], Response::HTTP_BAD_REQUEST);
+        }
         $user->setPassword($passwordHasher->hashPassword($user, $user->getPassword()));
+        $existingUser = $entityManager->getRepository(User::class)->findOneBy(['email' => $user->getEmail()]);
+        if ($existingUser) {
+            return new JsonResponse(['error' => 'Cet email est déjà utilisé'], Response::HTTP_CONFLICT);
+        }
         $entityManager->persist($user);
         $entityManager->flush();
         return new JsonResponse(['message' => 'User créé'], Response::HTTP_CREATED);
